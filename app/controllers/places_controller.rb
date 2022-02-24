b4acc1839de583ac5dc2cf556c39e1e6361d1bee
akfoury12 <alex.kfoury@kellogg.northwestern.edu>
2022-02-24 08:48:43 +0000

    2022.02.24 Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,23 @@
 class PlacesController < ApplicationController
+
+#here need to define certain actions
+
+def new
+@place = place.new
 end
+
+def 
+show @place = place.find(params["id"])
+end
+
+def create
+@place = Place.new(params["place"])
+@place.save
+redirect_to "/places"
+end
+
+def index
+@places = Place.ApplicationController
+end
+
+#covers places actions
